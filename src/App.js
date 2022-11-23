3051e10e0a4e8a6c725cbaee2236498b2ef53d5e
Hong Ly <lyhong.rupp@gmail.com>
2022-11-23 16:21:39 +1100

    react cheat sheet part 4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import Component from './Component';
+import "./App.css";
+import Component from "./Component";
+
+const people = [
+  { id: 1, name: "John", age: 15 },
+  { id: 2, name: "Jack", age: 20 },
+  { id: 3, name: "Jim", age: 35 },
+];
 
 function App() {
-  return (
-    <div>
-      {/* <Component name="Jack" age="25" /> */}
+  // Display a list of data
+  // return people.map((person) => <div key={person.id}>{person.name}</div>);
 
-      {/* children */}
-      {/* <Component>
-        John
-      </Component> */}
-
-      <Component />
-    </div>
-  );
+  // Pass a list of data to another component
+  return people.map(({ id, ...person }) => <Component key={id} {...person} />);
 }
 
 export default App;
--- src/Component.js
+++ src/Component.js
@@ -1,61 +1,24 @@
 import React from "react";
 
 // function Component(props) {
-//     return (
-//       <>
-//         <h1>
-//           Name: {props.name} Age: {props.age}
-//         </h1>
-//       </>
-//     );
-//   }
-
-// Destructured props
-// function Component({name, age}) {
 //   return (
 //     <>
 //       <h1>
-//         Name: {name} Age: {age}
+//         Name: {props.name} - Age: {props.age}
 //       </h1>
 //     </>
-//   )
+//   );
 // }
 
-// children
-// function Component({ children }) {
-//   return (
-//     <>
-//       <h1>
-//         Name: {children}
-//       </h1>
-//     </>
-//   )
-// }
-
-// Default props - JavaScript
-// function Component({ name = "Mike" }) {
-//   return (
-//     <>
-//       <h1>
-//         Name: {name}
-//       </h1>
-//     </>
-//   )
-// }
-
-// Default props - React
-function Component({ name }) {
+// Prop Destructuring
+function Component({ name, age }) {
   return (
     <>
       <h1>
-        Name: {name}
+        Name: {name}, Age: {age}
       </h1>
     </>
-  )
+  );
 }
 
-Component.defaultProps = {
-  name: "Hong"
-}
-  
 export default Component;
