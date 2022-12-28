778c6c5954e649962a26b38dcc025382188a0620
Hong Ly <lyhong.rupp@gmail.com>
2022-12-28 13:26:31 +1100

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,123 +1,44 @@
-import { useState, createContext, useContext } from "react";
+import { useReducer } from "react";
 
-// const ThemeContext = createContext(null);
+const initialState = { count: 0 };
 
-// function App() {
-//   return (
-//     <ThemeContext.Provider value="dark">
-//       <Component />
-//     </ThemeContext.Provider>
-//   );
-// }
-
-// function Component() {
-//   const theme = useContext(ThemeContext);
-//   return (
-//     <div>
-//       <p>The current theme is: {theme}</p>
-//     </div>
-//   );
-// }
-
-/////////
-
-// using useState
-// function App() {
-//   const [user, setUser] = useState("John");
-
-//   return (
-//     <>
-//       <h1>{`Hello ${user}!`}</h1>
-//       <Component2 user={user} />
-//     </>
-//   );
-// }
-
-// function Component2({ user }) {
-//   return (
-//     <>
-//       <h1>Component 2</h1>
-//       <Component3 user={user} />
-//     </>
-//   );
-// }
-
-// function Component3({ user }) {
-//   return (
-//     <>
-//       <h1>Component 3</h1>
-//       <Component4 user={user} />
-//     </>
-//   );
-// }
-
-// function Component4({ user }) {
-//   return (
-//     <>
-//       <h1>Component 4</h1>
-//       <Component5 user={user} />
-//     </>
-//   );
-// }
-
-// function Component5({ user }) {
-//   return (
-//     <>
-//       <h1>Component 5</h1>
-//       <h2>{`Hello ${user} again!`}</h2>
-//     </>
-//   );
-// }
-
-/////////
-
-const UserContext = createContext();
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
-  const [user, setUser] = useState("John");
+  // const [count, dispatch] = useReducer((state, action) => {
+  //   switch (action) {
+  //     case "increment":
+  //       return state + 1;
+  //     case "decrement":
+  //       return state - 1;
+  //     default:
+  //       throw new Error();
+  //   }
+  // }, 0);
 
-  return (
-    <UserContext.Provider value={user}>
-      <h1>{`Hello ${user}!`}</h1>
-      <Component2 />
-    </UserContext.Provider>
-  );
-}
+  // return (
+  //   <div>
+  //     <p>{count}</p>
+  //     <button onClick={() => dispatch("increment")}>+</button>
+  //     <button onClick={() => dispatch("decrement")}>-</button>
+  //   </div>
+  // );
 
-function Component2() {
-  return (
-    <>
-      <h1>Component 2</h1>
-      <Component3 />
-    </>
-  );
-}
-
-function Component3() {
-  return (
-    <>
-      <h1>Component 3</h1>
-      <Component4 />
-    </>
-  );
-}
-
-function Component4() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
-      <h1>Component 4</h1>
-      <Component5 />
-    </>
-  );
-}
-
-function Component5() {
-  const user = useContext(UserContext);
-
-  return (
-    <>
-      <h1>Component 5</h1>
-      <h2>{`Hello ${user} again!`}</h2>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
     </>
   );
 }
