0123ae8326a919bf28c9f22e0841a7bc414abf90
Hong Ly <lyhong.rupp@gmail.com>
2023-01-11 15:57:48 +1100

    useMemo Hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import { useState, useCallback } from "react";
-import Todos from "./Todos";
+import { useState } from "react";
+import Child from "./Child";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState([]);
-
-  const increment = () => {
-    setCount((c) => c + 1);
-  };
-
-  // const addTodo = () => {
-  //   setTodos((t) => [...t, "New Todo"]);
-  // };
-
-  // Use the useCallback Hook to prevent the Todos component from re-rendering if not neccessary
-  const addTodo = useCallback(() => {
-    setTodos((t) => [...t, "New Todo"]);
-  }, []);
+  const [num, setNum] = useState(1);
 
   return (
-    <>
-      <Todos todos={todos} addTodo={addTodo} />
-      <hr />
-      <div>
-        Count: {count}
-        <button onClick={increment}>+</button>
-      </div>
-    </>
+    <div>
+      <button onClick={() => setNum(num + 1)}>increment</button>
+      <Child number={num} />
+    </div>
   );
 }
 
--- src/Child.js
+++ src/Child.js
@@ -1,9 +1,17 @@
-function Child() {
+import { useMemo } from "react";
+
+function Child({ number }) {
+  // Use the useMemo hook to memoize the result of the expensive calculation
+  const memoizedDoubleNumber = useMemo(() => {
+    console.log("calculating doubleNumber");
+    return number * 2;
+  }, [number]);
+
   return (
     <div>
-      <p>The current theme is:</p>
+      <h1>Number: {number}</h1>
+      <h1>Double Number: {memoizedDoubleNumber}</h1>
     </div>
   );
 }
-
 export default Child;
